9d17b39e694cd645428afefe5770497238ec0cec
SoeunShin <21900395@handong.edu>
2023-12-05 10:18:34 +0900

    test

---
+++ src/main/java/com/example/cicdproj/CicDprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicDprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicDprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdproj/CicDprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
